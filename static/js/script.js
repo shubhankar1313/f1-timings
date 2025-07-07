7f8c45b0b5cf97c519c6654aae526a16d2e3cf4a
shubhankar1313 <shubhankarnath345@gmail.com>
2025-07-07 14:33:01 +0530

    indentation correction

--- static/js/script.js
+++ static/js/script.js
@@ -22,16 +22,16 @@ function initCustomTimezoneDropdown() {
       const label = option.textContent.trim();
 
       selected.innerHTML = `
-  <span class="label">${label}</span>
-  <img src="${flagSrc}" alt="">
-  <span class="arrow">
-    <svg width="12" height="12" viewBox="0 0 24 24" fill="none"
-         xmlns="http://www.w3.org/2000/svg">
-      <path d="M7 10l5 5 5-5" stroke="currentColor" stroke-width="2"
-            stroke-linecap="round" stroke-linejoin="round" />
-    </svg>
-  </span>
-`;
+        <span class="label">${label}</span>
+        <img src="${flagSrc}" alt="">
+        <span class="arrow">
+          <svg width="12" height="12" viewBox="0 0 24 24" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+            <path d="M7 10l5 5 5-5" stroke="currentColor" stroke-width="2"
+                  stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </span>
+      `;
 
       dropdown.classList.remove('open');
 
